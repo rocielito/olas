f304d01e372f36cdfc6e1b22bd6c3d6b7ea72143
lampantino <sre.quereck@gmail.com>
2015-12-25 22:42:01 +0100

    Resuelta la recarga del hashtag.

--- javascript/script.js
+++ javascript/script.js
@@ -27,7 +27,7 @@ $(function () {
   }(document, "script", "twitter-wjs");
 
   this.cargarInfo = function (hashtag, id) {
-    $('#info').find('.panel-body').html('<a class="twitter-timeline" href="https://twitter.com/hashtag/' + hashtag + '" data-widget-id="680475735954468864">Tweets sobre #' + hashtag + '</a>');
-    console.log(hashtag + ' / ' + id);
+    $('#info').find('.panel-body').html('<a class="twitter-timeline" href="https://twitter.com/hashtag/' + hashtag + '" data-widget-id="' + id + '"></a>Obteniendo información de Twitter...');
+    twttr.widgets.load();
   };
 });
