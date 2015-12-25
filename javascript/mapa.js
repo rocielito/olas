1309949521330d976f9d385936cc8b8bf43f8833
lampantino <sre.quereck@gmail.com>
2015-12-26 00:35:31 +0100

    Comenzada la lista de playas ordenadas por regiones para colocarla en el menú.

--- javascript/mapa.js
+++ javascript/mapa.js
@@ -11,6 +11,35 @@ $(function () {
 
   // Obtenemos las playas y añadimos los marcadores
   $.getJSON('./playas.json', function (data) {
+    // Añadimos las playas al menu
+    // Creamos un array que contiene todas las regiones ordenadas
+    var lista = [];
+    $.each(data.playas, function (i, playa) {
+      if ($.inArray(playa.region, lista) == -1) {
+        lista.push(playa.region);
+      }
+    });
+    lista.sort();
+    // Sustituimos cada región por un objeto al que añadirle playas
+    for (var i in lista) {
+      var region = {
+        region: lista[i],
+        playas: []
+      }
+      lista[i] = region;
+    }
+    // A cada región le añadimos sus playas correspondientes
+    $.each(data.playas, function (i, playa) {
+      for (var j in lista) {
+        if (lista[j].region == playa.region) {
+          lista[j].playas.push(playa.nombre);
+          lista[j].playas.sort();
+        }
+      }
+    });
+    console.log(lista);
+
+    // Añadimos los marcadores
     $.each(data.playas, function (i, playa) {
       var nombre = playa.nombre;
       var coordenadas = [playa.coordenadas[0], playa.coordenadas[1]];
@@ -20,9 +49,9 @@ $(function () {
       var surfForecast = playa.surfForecast;
 
       var contenido = '<b>Playa de ' + nombre + '</b>';
-      contenido += '<br><a href="#" onClick="cargarInfo(\'' + hashtag + '\', \'' + id + '\')">Info</a>';
-      //      contenido += '<br><a href="' + windguru + '" target="_blank">Windguru</a>';
-      //      contenido += '<br><a href="' + surfForecast + '" target="_blank">Surf Forecast</a>';
+      contenido += '<br><a href="#" onClick="cargarInfo(\'' + hashtag + '\', \'' + id + '\')">+Info (Twitter)</a>';
+      contenido += '<br><a href="' + windguru + '" target="_blank">Windguru</a>';
+      contenido += '<br><a href="' + surfForecast + '" target="_blank">Surf Forecast</a>';
       L.marker(coordenadas).addTo(map).bindPopup(contenido);
     });
   });
